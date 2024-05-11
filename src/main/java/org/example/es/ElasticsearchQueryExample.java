eab9a96a57858761c85078465dcfa02a9d06fdb6
DESKTOP-PIB4DJF\lisheng <524892125@QQ.COM>
2024-05-11 11:43:14 +0800

    es聚合搜索

--- src/main/java/org/example/es/ElasticsearchQueryExample.java
+++ src/main/java/org/example/es/ElasticsearchQueryExample.java
@@ -1,7 +1,6 @@
 package org.example.es;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.get.MultiGetRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -9,9 +8,12 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.script.Script;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -23,22 +25,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import static java.lang.System.exit;
+
 public class ElasticsearchQueryExample {
 
-    private static final String INDEX_NAME = "test_index"; // Replace with your index name
+    private static final String INDEX_NAME = "student"; // Replace with your index name
     private static final Random RANDOM = new Random();
 
-    public static void main(String[] args) {
-        // 创建客户端
-        RestClientBuilder builder = RestClient.builder(
-                new HttpHost("localhost", 9200, "http"));
-        RestHighLevelClient client = new RestHighLevelClient(builder);
+    public static void main(String[] args) throws IOException {
 
+            RestHighLevelClient client = createRestClientBuilder();
+        aggsAvg(client);
 
-
-
-
-//        insertRandomData(client, 5);
+//        insertRandomData(client, 100);
+        exit(1);
 
         // 创建查询请求
         SearchRequest searchRequest = new SearchRequest(INDEX_NAME); // 替换为你的索引名
@@ -48,7 +48,8 @@ public class ElasticsearchQueryExample {
 //        searchSourceBuilder.query(QueryBuilders.matchAllQuery()); // 这里是匹配所有文档
 
         // 根据字段查询
-        QueryBuilder queryBuilder = new MatchQueryBuilder("field1", "value23");
+//        QueryBuilder queryBuilder = new MatchQueryBuilder("field1", "value23");
+        QueryBuilder queryBuilder = QueryBuilders.matchQuery("field1", "value23");
         searchSourceBuilder.query(queryBuilder); // 这里是匹配所有文档
 
         // 设置查询构建器
@@ -75,6 +76,40 @@ public class ElasticsearchQueryExample {
         }
     }
 
+    public static RestHighLevelClient createRestClientBuilder() {
+        // 创建客户端
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost("192.168.9.128", 9200, "http"));
+        return new RestHighLevelClient(builder);
+    }
+
+    public static void aggsAvg (RestHighLevelClient client) throws IOException {
+        // 创建SearchRequest
+        SearchRequest searchRequest = new SearchRequest("student");
+
+        // 构建聚合
+        AvgAggregationBuilder avgAgeAgg = AggregationBuilders.avg("avg_age")
+                .field("age");
+
+        AvgAggregationBuilder avgAge188Agg = AggregationBuilders.avg("avg_age_188")
+                .script(new Script("doc['age'].value + 188"));
+
+        // 构建查询
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.size(0); // 不返回任何文档，只返回聚合结果
+        searchSourceBuilder.aggregation(avgAgeAgg);
+        searchSourceBuilder.aggregation(avgAge188Agg);
+
+        searchRequest.source(searchSourceBuilder);
+
+        // 执行查询
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 输出结果
+        System.out.println(response);
+    }
+
 
     private static void insertRandomData(RestHighLevelClient client, int numberOfDocuments) {
         for (int i = 0; i < numberOfDocuments; i++) {
@@ -92,9 +127,35 @@ public class ElasticsearchQueryExample {
 
     private static Map<String, Object> generateRandomDocument() {
         Map<String, Object> jsonMap = new HashMap<>();
-        jsonMap.put("field1", "value" + RANDOM.nextInt(100));
-        jsonMap.put("field2", RANDOM.nextInt(1000));
-        // Add more fields as needed
+
+        jsonMap.put("name", getRandomChineseString(RANDOM.nextInt(3)));
+        jsonMap.put("age", RANDOM.nextInt(100));
+        jsonMap.put("gender", RANDOM.nextBoolean() ? "male" : "female");
+        jsonMap.put("phone", RANDOM.nextLong());
+        jsonMap.put("email", getRandomChineseString(RANDOM.nextInt(3)) + "@example.com");
         return jsonMap;
+    }
+
+    /***
+     * 生成固定长度随机中文，kuojung
+     * @param n 中文个数
+     * @return 中文串
+     */
+    public static String getRandomChineseString(int n){
+        String zh_cn = "";
+        String str ="";
+
+        // Unicode中汉字所占区域\u4e00-\u9fa5,将4e00和9fa5转为10进制
+        int start = Integer.parseInt("4e00", 16);
+        int end = Integer.parseInt("9fa5", 16);
+
+        for(int ic=0;ic<n;ic++){
+            // 随机值
+            int code = (new Random()).nextInt(end - start + 1) + start;
+            // 转字符
+            str = new String(new char[] { (char) code });
+            zh_cn = zh_cn + str;
+        }
+        return zh_cn;
     }
 }
